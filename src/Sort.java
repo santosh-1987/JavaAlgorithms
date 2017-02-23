2306892a2c8047f857863f1c72b660d6e3da596f
Santosh Kumar Mohanty <santa.jyp@gmail.com>
2017-02-23 18:13:31 +0530

    Added Main Class

---
+++ src/Sort.java
@@ -0,0 +1,15 @@
+import sort.BubbleSort;
+
+public class Sort {
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java Algorithms !!!");
+    int unsortedArray[] = {15, 3, 9, 7, 19, 8, 1, 5};
+    System.out.println(unsortedArray);
+    System.out.println("Bubble Sort for ASC");
+    BubbleSort.sortAsc(unsortedArray);
+    System.out.println("Bubble Sort for Desc");
+    BubbleSort sort = new BubbleSort();
+    sort.sortDesc(unsortedArray);
+  }
+}
