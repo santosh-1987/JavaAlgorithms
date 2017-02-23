67d1a2682afe934d07a47db6218628eedb566a07
Santosh Kumar Mohanty <santa.jyp@gmail.com>
2017-02-23 18:04:50 +0530

    Added Bubble Sort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,48 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by mohantysan on 23/02/17.
+ */
+public class BubbleSort {
+
+  public static int[] sortAsc(int[] crunchifyArr) {
+    int temp;
+    for (int i = 0; i < crunchifyArr.length - 1; i++) {
+
+      for (int j = 1; j < crunchifyArr.length - i; j++) {
+        if (crunchifyArr[j - 1] > crunchifyArr[j]) {
+          temp = crunchifyArr[j - 1];
+          crunchifyArr[j - 1] = crunchifyArr[j];
+          crunchifyArr[j] = temp;
+        }
+      }
+      log("Iteration " + (i + 1) + ": " + Arrays.toString(crunchifyArr));
+    }
+    return crunchifyArr;
+  }
+
+  // Bubble Sort Algorithm in Descending Order
+  public  int[] sortDesc(int[] crunchifyArr) {
+    int temp;
+    for (int i = 0; i < crunchifyArr.length - 1; i++) {
+
+      for (int j = 1; j < crunchifyArr.length - i; j++) {
+        if (crunchifyArr[j - 1] < crunchifyArr[j]) {
+          temp = crunchifyArr[j - 1];
+          crunchifyArr[j - 1] = crunchifyArr[j];
+          crunchifyArr[j] = temp;
+        }
+      }
+      log("Iteration " + (i + 1) + ": " + Arrays.toString(crunchifyArr));
+    }
+    return crunchifyArr;
+  }
+
+  private static void log(String result) {
+    System.out.println(result);
+
+  }
+
+}
